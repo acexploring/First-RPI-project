e0c218d39602fa07057d2e3503014284b000b685
Akashi Chhaya <chhayaakashi@gmail.com>
2025-03-19 20:21:01 -0400

    added while loop

--- ledtest.py
+++ ledtest.py
@@ -1,4 +1,17 @@
 import RPi.GPIO as GPIO
-GPIO .setmode(GPIO.BOARD)
+import time
+
+# Set up the GPIO pin
+GPIO.setmode(GPIO.BOARD)
 GPIO.setup(11, GPIO.OUT)
-GPIO.output(11, GPIO.1)
+
+# Flash the LED for 1 minute
+start_time = time.time()
+while time.time() - start_time < 60:  # Run for 60 seconds
+    GPIO.output(11, 1)  # Turn LED on
+    time.sleep(1)     # Wait 0.5 seconds
+    GPIO.output(11, 0)  # Turn LED off
+    time.sleep(1)     # Wait 0.5 seconds
+
+# Clean up GPIO settings
+GPIO.cleanup()
